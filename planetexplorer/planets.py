742ac22900efcd62ca29eafc7abe6c03e7903211
jhocoder <jhonathanrugeb@gmail.com>
2025-02-09 14:55:09 +0100

    prisma ORM added

---
+++ planetexplorer/planets.py
@@ -0,0 +1,6 @@
+class Planets():
+    def __init__(self, name, temperatura, gravedad, habitabilidad, composicion):
+        self.name = name
+        self. temperatura = temperatura
+        self.gravedad = gravedad
+        self.habitabilidad = 
